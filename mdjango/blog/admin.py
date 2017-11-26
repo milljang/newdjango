1355217716b51a3beddb50efb926ea3d1ffaa5f6
newzen <erp2001@naver.com>
2017-11-26 16:46:01 +0900

    post 내용추가
    admin의 제목과 생성날짜추가

--- mdjango/blog/admin.py
+++ mdjango/blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post,comment,Tag
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content','created_at', 'updated_at']
+    list_display_links = ['id', 'title']
+#    list_editable = ['author']
+#    list_per_page = 3
+#    list_filter = ['author', 'created_at']
+
+
+@admin.register(comment)
+class commentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display =['name']
--- mdjango/blog/models.py
+++ mdjango/blog/models.py
@@ -1,3 +1,32 @@
+import re
 from django.db import models
+from django.forms import ValidationError
 
-# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100,verbose_name='제목', #국제화 안됨
+          choices = (
+               ('제목1', '제목 1 레이블'), # 실제 저장될값 2. 보여주는값
+               ('제목2', '제목 2 레이블'),
+               ('제목3', '제목 3 레이블'),
+          ))
+    content = models.TextField(verbose_name='내용')
+    tags = models.CharField(max_length=100, blank=True)
+
+    tag_set = models.ManyToManyField('Tag',blank=True)
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name='작성일시')
+    updated_at = models.DateTimeField(auto_now=True,verbose_name='수정일시')
+
+class comment (models.Model):
+    post = models.ForeignKey(Post)
+    author = models.CharField(max_length=20)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
